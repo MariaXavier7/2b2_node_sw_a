18e28ce19263cea12479c7ce5ea77843be044a0d
@Maria.Silva5408@etec.sp.gov.br <@Maria.Silva5408.etec.sp.gov.br>
2025-02-25 18:20:58 -0300

    funções node

---
+++ aula2/index.js
@@ -0,0 +1,19 @@
+function soma(n1, n2){
+     return (n1 + n2)
+    }
+
+    function div(n1, n2){
+        return (n1 / n2)
+       }
+
+       function sub(n1, n2){
+        return (n1 - n2)
+       }
+
+       function mut(n1, n2){
+        return (n1 * n2)
+       }
+    
+       console.log('O ')
+ 
+   
